44559387d9a06e85df6c7e61959df85739adab4a
darya <dasha.andreyuk@gmail.com>
2024-07-29 22:38:35 +0200

    updated StandaloneTest file

--- src/test/java/rahulshettyacademy/tests/StandaloneTest.java
+++ src/test/java/rahulshettyacademy/tests/StandaloneTest.java
@@ -18,6 +18,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class StandaloneTest {
 
 	public static void main(String[] args) {
+		//new comment
 		String productName = "ZARA COAT 3";
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
