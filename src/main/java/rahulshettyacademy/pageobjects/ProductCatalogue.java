b6c0d39a1b09cb73217f2139f07d77815d111464
Darya Andreyuk <161613472+DaryaAndreyuk@users.noreply.github.com>
2024-10-02 15:56:22 +0200

    Update ProductCatalogue.java

--- src/main/java/rahulshettyacademy/pageobjects/ProductCatalogue.java
+++ src/main/java/rahulshettyacademy/pageobjects/ProductCatalogue.java
@@ -1,13 +1,11 @@
 package rahulshettyacademy.pageobjects;
 
 import java.util.List;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
 import rahulshettyacademy.AbstractComponents.AbstractComponent;
 
 public class ProductCatalogue extends AbstractComponent {
@@ -15,13 +13,11 @@ public class ProductCatalogue extends AbstractComponent {
 	WebDriver driver;
 
 	public ProductCatalogue(WebDriver driver) {
-		// initialization
 		super(driver);
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
 	}
 
-	// PageFactory
 	@FindBy(css = ".mb-3")
 	List<WebElement> products;
 	
@@ -52,5 +48,4 @@ public class ProductCatalogue extends AbstractComponent {
 		waitForElementToAppear(toastMessage);
 		waitForElementToDisappear(spinner);
 	}
-
 }
