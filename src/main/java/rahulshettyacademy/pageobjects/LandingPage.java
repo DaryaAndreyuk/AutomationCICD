d67fa8b23f0e0269d61c44e5480b1d7c338a6cab
Darya Andreyuk <161613472+DaryaAndreyuk@users.noreply.github.com>
2024-10-02 15:55:31 +0200

    Update LandingPage.java

--- src/main/java/rahulshettyacademy/pageobjects/LandingPage.java
+++ src/main/java/rahulshettyacademy/pageobjects/LandingPage.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
 import rahulshettyacademy.AbstractComponents.AbstractComponent;
 
 public class LandingPage extends AbstractComponent {
@@ -41,12 +40,10 @@ public class LandingPage extends AbstractComponent {
 
 	public void goTo() {
 		driver.get("https://rahulshettyacademy.com/client");
-
 	}
 	
 	public String getErrorMessage() {
 		waitForWebElementToAppear(errorMessage);
 		return errorMessage.getText();
 	}
-
 }
